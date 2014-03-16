101ed7e81bc6d20c0460732ed7cecbf169503a60
Ricardo Havill <rhavill@progresstesting.com>
2014-03-16 16:21:24 -0400

    Modified login form controller to broadcast the event after a login attempt.

--- assets/linker/custom/common/login/loginForm.js
+++ assets/linker/custom/common/login/loginForm.js
@@ -6,7 +6,7 @@ angular.module('login.form', ['ui.directives'])
     })
 // The LoginFormController provides the behaviour behind a reusable form to allow users to authenticate.
 // This controller and its template (login/form.tpl.html) are used in a modal dialog box by the security service.
-    .controller('LoginFormController', ['$scope', 'login', function ($scope, login) {
+    .controller('LoginFormController', ['$scope', '$rootScope', 'login', function ($scope, $rootScope, login) {
       // The model for this form
       $scope.user = null;
       $scope.alerts = [];
@@ -20,22 +20,25 @@ angular.module('login.form', ['ui.directives'])
       };      
 
       // Attempt to authenticate the user specified in the form's model
-      $scope.login = function () {
+      $scope.login = function (user) {
 
         // Try to login
-        login.login($scope.user.username, $scope.user.password).then(function () {
+        login.login(user.email, user.password).then(function () {
           if (!login.isAuthenticated()) {
             // If we get here then the login failed due to bad credentials
             $scope.user = null;
-            $scope.addAlert({msg: $scope.authError, type: 'danger'});
+            //$scope.addAlert({msg: $scope.authError, type: 'danger'});
+            $rootScope.$broadcast('login-failed');
           }
           else {
             $scope.user = login.user;
+            $rootScope.$broadcast('login-success');
           }
         }, function (x) {
           // If we get here then there was a problem with the login request to the server
           $scope.user = null;
-          $scope.authError = 'Error logging in. Problem contacting server.';
+          //$scope.authError = 'Error logging in. Problem contacting server.';
+          $rootScope.$broadcast('login-server-error');
         });
       };
 
